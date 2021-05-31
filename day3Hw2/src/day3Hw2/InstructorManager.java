b1cba4b6d8618b3ad0a1e2c040991de18f9f1780
Burçak GÜNERİ <burcakguneri@outlook.com>
2021-05-31 20:10:35 +0300

    Delete day3Hw2/src/day3Hw2 directory

--- day3Hw2/src/day3Hw2/InstructorManager.java
+++
@@ -1,8 +0,0 @@
-package day3Hw2;
-
-public class InstructorManager {
-
-	public void add(Instructor instructor) {
-		System.out.println(instructor.getFirstName() + instructor.getLastName() + " " + "Java yazýlým geliþtirme eðitimine atandý.");
-	}
-}
