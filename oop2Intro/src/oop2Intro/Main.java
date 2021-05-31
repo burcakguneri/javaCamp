b7af0d7b1e51e8f05e107b86abb0daf90d0f8ff4
Burcak <Burcak@DESKTOP-2TJTQ03>
2021-05-31 19:57:43 +0300

    oop2Intro

--- oop2Intro/src/oop2Intro/Main.java
+++ oop2Intro/src/oop2Intro/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB Ram", 10); // referans oluþturma, intance
-
+		//Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB Ram", 10); // referans oluþturma, intance
+		Product product1 = new Product();
+		product1.setId(1);
+		product1.setName("Lenovo V14");
+		product1.setDetail("16 Gb Ram");
+		product1.setUnitPrice(15000);
+		product1.setDiscount(10);
 		Product product2 = new Product();
 		product2.setId(1);
 		product2.setName("Lenovo V15");
